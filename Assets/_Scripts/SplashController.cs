8ebc29d5901c80c7f20d017e2abbbde1f10d0f36
tim-hub <tim_w@outlook.com>
2015-10-05 19:45:40 +0800

    splash optimiztion, and delete some file

--- Assets/_Scripts/SplashController.cs
+++ Assets/_Scripts/SplashController.cs
@@ -4,19 +4,21 @@ using System.Collections;
 public class SplashController : MonoBehaviour {
 	public string nextLevel;
 	
-	// Update is called once per frame
-	void Update () {
-#if UNITY_EDITOR
+	public void NextLevel(){ //click button 
+		Application.LoadLevel(nextLevel);
+	}
 
+	public void Update(){ //or click screen
+#if UNITY_EDITOR
 		if(Input.GetMouseButton(0)){
-			Application.LoadLevel(nextLevel);
+
+			NextLevel();
 		}
 #elif UNITY_ANDROID
-
-		if(Input.touchCount>0){ //when screen was touched
-
-			Application.LoadLevel(nextLevel);
+		if(Input.TouchCount>=1){
+			NextLevel();
 		}
+
 #endif
 	}
 }
