2954719387520bdfb4f17efa47e42c41267cf083
tim-hub <tim_w@outlook.com>
2015-10-08 16:50:53 +0800

    export to android studio

---
+++ src/uno/bai/geekgame/UnityPlayerProxyActivity.java
@@ -0,0 +1,24 @@
+package uno.bai.geekgame;
+
+import com.unity3d.player.*;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * @deprecated Use UnityPlayerActivity instead.
+ */
+public class UnityPlayerProxyActivity extends Activity
+{
+	protected void onCreate (Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+
+		Intent intent = new Intent(this, uno.bai.geekgame.UnityPlayerActivity.class);
+		intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+		Bundle extras = getIntent().getExtras();
+		if (extras != null)
+			intent.putExtras(extras);
+		startActivity(intent);
+	}
+}
